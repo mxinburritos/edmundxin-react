c86550d834af57ef615333f067ece2e9b0edadfb
Edmund Xin <mxinburritos@gmail.com>
2020-05-29 17:34:21 -0700

    CardLayout Own Component

--- src/components/ProjectCards/CardLayout.jsx
+++ src/components/ProjectCards/CardLayout.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-import { Card, Grid, Box } from "@material-ui/core";
-import Masonry from "react-masonry-component";
+import React from 'react';
+import { Card, Grid, Box, CardMedia } from '@material-ui/core';
+import Masonry from 'react-masonry-component';
 
-import ProjectCard from "./ProjectCard";
+import ProjectCard from './ProjectCard';
 
 const masonryOptions = {
-	transitionDuration: 0,
+  transitionDuration: 0,
 };
 
 const CardLayout = () => {
-	return (
-		<React.Fragment>
-			<h1>Projects</h1>
+  return (
+    <CardMedia>
+      <h1>Projects</h1>
 
-			<Box
-				m={2}
-				display="flex"
-				flexDirection="row"
-				flexWrap="nowrap"
-				justifyContent="center"
-			>
-				<ProjectCard />
-				<ProjectCard />
-				<ProjectCard />
-			</Box>
-			<Box
-				m={2}
-				display="flex"
-				flexDirection="row"
-				flexWrap="nowrap"
-				justifyContent="center"
-			>
-				<ProjectCard />
-				<ProjectCard />
-				<ProjectCard />
-			</Box>
-		</React.Fragment>
-	);
+      <Box
+        m={2}
+        display="flex"
+        flexDirection="row"
+        flexWrap="nowrap"
+        justifyContent="center"
+      >
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </Box>
+      <Box
+        m={2}
+        display="flex"
+        flexDirection="row"
+        flexWrap="nowrap"
+        justifyContent="center"
+      >
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </Box>
+    </CardMedia>
+  );
 };
 
 export default CardLayout;
